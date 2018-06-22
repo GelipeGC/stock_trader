6259c5acad734aa5084fa089fd0c353a7290f3b7
Felipe Guzman <felipe.lightcone@gmail.com>
2018-06-22 10:50:21 -0500

    stock trader app

---
+++ src/components/ViewTree.vue
@@ -0,0 +1,49 @@
+<template>
+  <ul id="app">
+    <app-item
+      class="item"
+      :model="treeData">
+    </app-item>
+  </ul>
+</template>
+
+<script>
+import Item from './Tree.vue';
+  export default {
+    components: {
+      appItem: Item
+    },
+    data () {
+      return {
+        treeData: {
+            name: 'My Tree',
+            children: [
+              { name: 'hello' },
+              { name: 'wat' },
+              {
+                name: 'child folder',
+                children: [
+                  {
+                    name: 'child folder',
+                    children: [
+                      { name: 'hello' },
+                      { name: 'wat' }
+                    ]
+                  },
+                  { name: 'hello' },
+                  { name: 'wat' },
+                  {
+                    name: 'child folder',
+                    children: [
+                      { name: 'hello' },
+                      { name: 'wat' }
+                    ]
+                  }
+                ]
+              }
+            ]
+          }
+      }
+    }
+  } 
+</script> 
